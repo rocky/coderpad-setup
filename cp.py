6e1f6318dac9ca09c36db1e64045825351d9f1fd
rocky <rb@dustyfeet.com>
2019-09-30 09:58:27 -0400

    More stuff

--- cp.py
+++ cp.py
@@ -2,4 +2,5 @@ import os; import sys; import requests
 sh=os.system
 sh("pip3 install trepan3k")
 sys.path.append(".local/lib/python3.6/site-packages")
+import trepan
 print("Rocky's coderpad setup added")
