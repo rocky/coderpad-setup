3e6600859c7f5a3a4e5473bd3e4175ade8f6726b
rocky <rb@dustyfeet.com>
2019-09-30 12:01:09 -0400

    Add lp() and trepanmagic

--- cp.py
+++ cp.py
@@ -1,6 +1,8 @@
 import os; import sys; import requests
+# import requests; exec(requests.get("https://raw.githubusercontent.com/rocky/coderpad-setup/master/cp.py").content)
 sh=os.system
+lp=lambda program="solution.py": exec(open(program).read())
 sh("pip3 install trepan3k")
 sys.path.append(".local/lib/python3.6/site-packages")
-import trepan
-print("Rocky's coderpad setup added")
+exec(requests.get("https://raw.github.com/rocky/ipython-trepan/master/trepanmagic.py").content)
+print("Rocky's coderpad setup added.")
