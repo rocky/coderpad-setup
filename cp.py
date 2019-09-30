1f17587de0762918e2b40bcd99ac0bd0a018c5be
rocky <rb@dustyfeet.com>
2019-09-30 12:04:46 -0400

    Fix lp()

--- cp.py
+++ cp.py
@@ -1,7 +1,8 @@
 import os; import sys; import requests
 # import requests; exec(requests.get("https://raw.githubusercontent.com/rocky/coderpad-setup/master/cp.py").content)
 sh=os.system
-lp=lambda program="solution.py": exec(open(program).read())
+def lp(program="solution.py"):
+    exec(open(program).read())
 sh("pip3 install trepan3k")
 sys.path.append(".local/lib/python3.6/site-packages")
 exec(requests.get("https://raw.github.com/rocky/ipython-trepan/master/trepanmagic.py").content)
